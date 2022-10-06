0f0566915b631238858ef2c77e8ba05b6450b581
roeez <Roeezach15@gmail.com>
2022-10-06 23:19:59 +0300

    change root folder path

--- ExpensesManger.Services/BuisnessLogic/Integrations/Models/SplitWiseModels/CategoryItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExpensesManager.Integrations.SplitWiseModels
-{
-    public class CategoryItem
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-    }
-}
--- ExpensesManger.Services/BuisnessLogic/Integrations/Models/SplitWiseModels/RepaymentsItems.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExpensesManager.Integrations.SplitWiseModels
-{
-    public class RepaymentsItems
-    {
-        public int From { get; set; }
-        public int To { get; set; }
-        public string Amount { get; set; }
-    }
-}
