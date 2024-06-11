bfb4c72eb145c6c99b0829da297d152b7fc4da70
roee zach <roeezach15@gmail.com>
2024-06-11 13:06:07 +0300

    start building design pattern factory and use exelicdraw for explanantion

---
+++ Backend/ExpensesManager.Services/BuisnessLogic/Map/Factories/HapoalimExpenseMapperFactory.cs
@@ -0,0 +1,11 @@
+using ExpensesManager.Services;
+using ExpensesManager.Services.BuisnessLogic.Map;
+using ExpensesManager.Services.BuisnessLogic.Map.ExpenseMappers
+
+    public class HapoalimExpenseMapperFactory : IExpenseMapperFactory
+{
+    public ExpenseMapper CreateExpenseMapper()
+    {
+        return new HapoalimExpenseMapper();
+    }
+}
