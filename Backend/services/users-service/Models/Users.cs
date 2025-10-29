35d22d9da86db9e1cbd409d15c62de6edcf614c0
roee zach <roeezach15@gmail.com>
2025-10-29 18:24:23 +0200

    feat(users): add Users model and UsersDbContext with tests

---
+++ Backend/services/users-service/Models/Users.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpensesManager.UsersService.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        [Column("Username", Order = 2)]
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int CreditCardChargeDay { get; set; }
+        public string SplitwisePassword { get; set; }
+        public string SW_User_ID { get; set; }
+    }
+}
